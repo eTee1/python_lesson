196608d63077dd56085a230bc8ddc4d92254cc1a
Michael Zborovski <ukrainemts@gmail.com>
2022-07-29 11:28:50 +0300

    gdfg

---
+++ lesson4/apples.py
@@ -0,0 +1,6 @@
+pupils = int(input("How many pupils get apples:\n"))
+apples = int(input("How many apples:\n"))
+var1 = apples // pupils
+var2 = apples % pupils
+print("Every pupil got", var1, "apples")
+print("In basket left", var2, "apples")
