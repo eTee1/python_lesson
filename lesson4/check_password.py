3e14df8a71316d9357d3039b186eaabdd2f0b059
Michael Zborovski <ukrainemts@gmail.com>
2022-07-29 12:27:21 +0300

    #3 verify_password

--- lesson4/check_password.py
+++ lesson4/check_password.py
@@ -2,6 +2,6 @@ password1 = str(input("Enter password:\n"))
 pwd1 = ("12345", "abrakadabra", "weeeee", "pass123")
 
 if password1 in pwd1:
-        print("Access granted")
+    print("Access granted")
 else:
-        print("Access denied")
+    print("Access denied")
