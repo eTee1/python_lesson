cffc790c4791e03465a37962b905bb042df3c772
Michael Zborovski <ukrainemts@gmail.com>
2022-07-29 11:30:13 +0300

    apples

--- lesson4/apples.py
+++ lesson4/apples.py
@@ -3,4 +3,4 @@ apples = int(input("How many apples:\n"))
 var1 = apples // pupils
 var2 = apples % pupils
 print("Every pupil got", var1, "apples")
-print("In basket left", var2, "apples")
+print("In basket left", var2, "apples");
