4663c37ad01691e5b407e251d135e40f210fb058
Michael Zborovski <ukrainemts@gmail.com>
2022-07-31 14:55:58 +0300

    #4 leap year calendar check

--- lesson4/calendar.py
+++ lesson4/calendar.py
@@ -2,7 +2,7 @@ year = int(input("Enter year in format (YYYY):"))
 if year <= 1900 or year >= 1_000_000:
     print("Year not match requirements!")
 else:
-    if year % 4 > 0 or year % 100 > 0 or year % 400 > 0:
+    if year % 4 > 0 or year % 100 == 0 and year % 400 > 0:
         print("Year is not leap")
     else:
         print("Year is leap")
