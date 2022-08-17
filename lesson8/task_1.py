f807ce0378b282e184fc44146ee2d18fbd52c2cf
Michael Zborovski <ukrainemts@gmail.com>
2022-08-17 18:57:59 +0300

    .

--- lesson8/task_1.py
+++ lesson8/task_1.py
@@ -2,7 +2,7 @@ matrix = int(input('Enter positive value size of matrix NxN N= '))
 for row in range(1, matrix + 1):
     for col in range(1, matrix + 1):
         if row % 2 == 0:
-            print('%4d' % row, end=' ')  # использовал 4 для длинны учитывая "-", тк матрицу N=999 строит минуту
+            print('%4d' % row, end='\t') #матрицы N > 999+ долго строит, ограничил включая минус
         else:
-            print('%4d' % (col - 1 - matrix), end=' ')
+            print('%4d' % (col - 1 - matrix), end='\t')
     print()
